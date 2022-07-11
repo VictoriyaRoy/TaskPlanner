64416012a0ca356b4dee9a580c4401f76b23856a
VictoriyaRoy <victoriyaroi2003@gmail.com>
2022-07-11 15:56:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tasks/data/model/Task.kt
@@ -0,0 +1,8 @@
+package com.example.tasks.data.model
+
+class Task(
+    val title: String,
+    val description: String,
+    val category: String,
+    val priority: String
+)
---
+++ app/src/main/java/com/example/tasks/ui/list/ListFragment.kt
@@ -0,0 +1,29 @@
+package com.example.tasks.ui.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.tasks.R
+import com.example.tasks.data.model.Task
+import com.example.tasks.ui.adapters.TaskAdapter
+import kotlinx.android.synthetic.main.fragment_list.*
+import kotlinx.android.synthetic.main.fragment_list.view.*
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+        val testTask = Task("Title", "Description", "Category", "Priority")
+        val adapter = TaskAdapter(listOf(testTask))
+        view.tasksRecycler.layoutManager = LinearLayoutManager(activity)
+        view.tasksRecycler.adapter = adapter
+        return view
+    }
+
+}
